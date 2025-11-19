459b1cdfe043ca2b21283201c98bb40e73d6ea2f
Guilherme Renan Guedes Sousa <renanguedes_jenipa@hotmail.com>
2025-11-19 02:30:32 -0300

    ajuste no pr

--- src/Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
+++ src/Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4db9871404958433e01e5acf66021a6c4c51dcb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cb6e62ccf71567dcffa7b9c75ecb636b40cb99d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
+++ src/Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4db9871404958433e01e5acf66021a6c4c51dcb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cb6e62ccf71567dcffa7b9c75ecb636b40cb99d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
