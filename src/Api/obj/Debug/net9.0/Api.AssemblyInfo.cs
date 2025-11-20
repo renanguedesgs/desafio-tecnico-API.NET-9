229020b1a026fa35c4d49054ede63727df06e0f2
Guilherme Renan Guedes Sousa <renanguedes_jenipa@hotmail.com>
2025-11-20 00:59:17 -0300

    pr main

--- src/Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
+++ src/Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3fc796ec65cd50ef19dcee5af255fa4cdbee898")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8379bc51862f371e3549bdab00990dc311d29562")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
