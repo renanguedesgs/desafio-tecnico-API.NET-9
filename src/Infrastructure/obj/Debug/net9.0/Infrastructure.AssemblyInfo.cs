2d555f8965e1b0b4a1cd571bf15565448ffc7eee
Guilherme Renan Guedes Sousa <renanguedes_jenipa@hotmail.com>
2025-11-19 23:38:29 -0300

    pr main

--- src/Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
+++ src/Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abba95ae85ce3d662b2ace0fb4ce9876b2c174d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b5a740434b17cf8a9b7f0d594539b25051e6164")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
