c5f232509441bcda974dda987164372caf213bf6
Guilherme Renan Guedes Sousa <renanguedes_jenipa@hotmail.com>
2025-11-20 13:17:37 -0300

    Ajuste no PR

--- src/Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
+++ src/Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bf17f9497ccbef0ffca948f95667ebe2491518f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2eb731135508a926ae07f69ef3236ed14b500ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
