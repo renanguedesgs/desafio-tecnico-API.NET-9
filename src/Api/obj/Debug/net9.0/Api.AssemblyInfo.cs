44c7809732aa5c827545f047ac97244d105bc2df
Guilherme Renan Guedes Sousa <renanguedes_jenipa@hotmail.com>
2025-11-21 01:15:42 -0300

    main

--- src/Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
+++ src/Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d1d90c79b5108029bdd7f4ffeaf3a0584f24b52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c004b93adec253324fda0834767d6451a33150d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
